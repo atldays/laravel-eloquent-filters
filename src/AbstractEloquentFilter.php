9a2c227d703f36cbb0ff58bf1a8ea9afac5ff58e
Anjey Tsibylskij <130153594+atldays@users.noreply.github.com>
2026-04-11 22:29:07 +0300

    feat: enhance query filtering with added docblocks and iterable support

--- src/AbstractEloquentFilter.php
+++ src/AbstractEloquentFilter.php
@@ -7,8 +7,20 @@ use Illuminate\Database\Eloquent\Builder;
 
 abstract class AbstractEloquentFilter implements EloquentFilterContract
 {
+    /**
+     * Applies modifications to the given query builder instance.
+     *
+     * @param  Builder  $query  The query builder instance to be modified.
+     * @return Builder The modified query builder instance.
+     */
     abstract public function apply(Builder $query): Builder;
 
+    /**
+     * Determines if the current context or configuration satisfies the necessary conditions
+     * for applicability.
+     *
+     * @return bool Returns true if applicable, false otherwise.
+     */
     public function isApplicable(): bool
     {
         return true;
--- src/Filterable.php
+++ src/Filterable.php
@@ -4,17 +4,18 @@ namespace Atldays\EloquentFilters;
 
 use Atldays\EloquentFilters\Contracts\EloquentFilterContract;
 use Illuminate\Database\Eloquent\Builder;
+use Throwable;
 
 trait Filterable
 {
     /**
-     * @param  EloquentFilters|EloquentFilterContract|EloquentFilterContract[]  $filters
+     * @throws Throwable
      */
-    public function scopeFilter(Builder $query, EloquentFilters|EloquentFilterContract|array $filters): Builder
+    public function scopeFilter(Builder $query, EloquentFilters|EloquentFilterContract|iterable $filters): Builder
     {
         if ($filters instanceof EloquentFilterContract) {
             $filters = EloquentFilters::make([$filters]);
-        } elseif (is_array($filters)) {
+        } elseif (is_iterable($filters)) {
             $filters = EloquentFilters::make($filters);
         }
 
